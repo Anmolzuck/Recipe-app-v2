afedf4838d94a07d4f38a53ecb2824b22819c0ab
Anmol Zakie <anmolzuck17@gmail.com>
2020-12-12 19:20:40 +0530

    Version 2 of forkify project😀

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '917594c4-dcba-4480-b0e7-beb24e0f1e5f';
+export const MODAL_CLOSE_SEC = 2.5;
